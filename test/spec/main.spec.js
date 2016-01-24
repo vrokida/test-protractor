e59187c3a74fbf0f2b6c6b0a94c20e55c2eca6ae
vrokida <vrokida@gmail.com>
2016-01-24 11:03:42 -0500

    fc

--- test/spec/main.spec.js
+++ test/spec/main.spec.js
@@ -8,7 +8,7 @@ describe('angularjs homepage', function() {
 
     var greeting = element(by.binding('yourName'));
 
-    expect(greeting.getText()).toEqual('Hello Julie!');
+    expect(greeting.getText()).toEqual('Hello vero!');
   });
 
   describe('todo list', function() {
