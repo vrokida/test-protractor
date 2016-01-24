7d680876cbda722e42dec5739c27c3947c2ee75d
vrokida <vrokida@gmail.com>
2016-01-23 22:51:59 -0500

    fc

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -25,14 +25,17 @@ module.exports = function(config) {
       'bower_components/jquery/dist/jquery.js',
       'bower_components/angular/angular.js',
       'bower_components/bootstrap/dist/js/bootstrap.js',
+      'bower_components/angular-animate/angular-animate.js',
       'bower_components/angular-cookies/angular-cookies.js',
       'bower_components/angular-resource/angular-resource.js',
       'bower_components/angular-route/angular-route.js',
+      'bower_components/angular-sanitize/angular-sanitize.js',
+      'bower_components/angular-touch/angular-touch.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
       "test/mock/**/*.js",
-      "test/spec/**/*.js"
+      "test/spec/controllers/*.js"
     ],
 
     // list of files / patterns to exclude
